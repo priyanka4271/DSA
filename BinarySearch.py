35ca70347ef9d49e8303dce34cdb2e47df35eac3
priyanka4271 <ps9673932@gmail.com>
2025-03-08 21:13:52 +0530

    change in new file

---
+++ BinarySearch.py
@@ -0,0 +1,20 @@
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+
+
+arr = [1, 3, 5, 7, 9]
+target = 5
+print("element is found at index")
+print(binary_search(arr, target))
